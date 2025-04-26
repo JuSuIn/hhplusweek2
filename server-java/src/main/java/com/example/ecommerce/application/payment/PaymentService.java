17872309abb05ccbdc4e91804336c4e58aeef80f
SUINJU <jusuin5876@naver.com>
2025-04-26 19:40:28 +0900

    3주차 미구현된 결제 로직 구현(도메인,서비스)

--- server-java/src/main/java/com/example/ecommerce/application/payment/PaymentService.java
+++ server-java/src/main/java/com/example/ecommerce/application/payment/PaymentService.java
@@ -1,1 +1,45 @@
-// 결제 도메인 관련 호출,트랜잭션 제어
+import com.example.ecommerce.domain.payment.Payment;
+import com.example.ecommerce.domain.payment.PaymentMethod;
+import com.example.ecommerce.domain.payment.PaymentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 결제 처리 서비스
+ */
+
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    /**
+     * 결제 생성 (결제 요청)
+     */
+    @Transactional
+    public Payment createPayment(Long orderId, Long amount, PaymentMethod menthod){
+        Payment payment = new Payment(orderId,amount,menthod);
+        return paymentRepository.save(payment);
+    }
+    /**
+     * 결제 완료 처리
+     */
+    @Transactional
+    public void completePayment(Long paymentId){
+        Payment payment = paymentRepository.findById(payementId)
+                .orElseThrow( () -> new EntityNotFoundException("결제 정보를 찾을 수 없습니다."));
+
+        payment.markAsPaid();
+    }
+
+    /**
+     * 결제 실패 처리
+     * */
+    @Transactional
+    public void failPayment(Long paymentId){
+        Payment payment = paymentRepository.findById(paymentId)
+                .orElseThrow( () -> new EntityNotFoundException("결제 정보를 찾을 수 없습니다."));
+
+        payment.markAdFailed();
+    }
+
+}
--- server-java/src/main/java/com/example/ecommerce/domain/payment/Payment.java
+++ server-java/src/main/java/com/example/ecommerce/domain/payment/Payment.java
@@ -8,6 +8,8 @@ package com.example.ecommerce.domain.payment;// 주문에 대해 얼마를, 어�
 import com.example.ecommerce.domain.order.Order;
 import com.example.ecommerce.domain.payment.PaymentStatus;
 import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
@@ -18,22 +20,47 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private PaymentMethod paymentMethod; // 예: CARD, KAKAO_PAY, NAVER_PAY
-    private int amount;
+    private Long orderId;//결제 대상 주문ID
 
     @Enumerated(EnumType.STRING)
-    private PaymentStatus status;
+    private PaymentMethod method; // 결제 수단 (카드, 계좌이체 등)
 
-    private LocalDateTime paidAt;
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status; // 결제 상태 (결제 완료, 실패 등)
+
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
-    private Order order;
+    private Order order;            //주문
 
+    private Long amount; //결제 금액
 
-    // 결제수단 변경
+    @CreatedDate
+    private LocalDateTime paidAt;// 결제 완료 일시
+
+    protected Payment() {}
+
+    public Payment(Long orderId,Long amount,PaymentMethod method){
+        this.orderId=orderId;
+        this.amount=amount;
+        this.method=method;
+        this.status=PaymentStatus.READY; //처음상태(결제 대기)
+    }
+
+    //결제 완료 처리
+    public void markAsPaid(){
+        this.status=PaymentStatus.COMPLETED;
+        this.paidAt= LocalDateTime.now();
+    }
+
+    //결젶 실패 처리
+    public void markAdFailed(){
+        this.status = PaymentStatus.FAILED;
+    }
+
+    // 결제 수단 변경
     public void validdatePaymentMethod(){
-        this.paymentMethod.validateAvailability();
+        this.method.validateAvailability();
     }
 
 }
